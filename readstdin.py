e727423a8f71c32160dadc9929de7a9910cd1243
Fredrik Dahlberg <fd@abc.se>
2019-03-02 17:51:01 +0100

    Somewhat working

--- readstdin.py
+++ readstdin.py
@@ -1,6 +1,7 @@
 import csv
 import sys
 import datetime
+import re
 
 def isdate(string):
     try:
@@ -19,7 +20,25 @@ for line in csvfile:
             date = datetime.datetime.strptime(field, '%Y-%m-%d')
             desc = next(line)
             category = next(line)
-            amount = next(line)
+            amount = float(next(line).replace(".", "").replace(",", "."))
             saldo = next(line)
+            try:
+                date = datetime.datetime.strptime(desc, 'Kortköp %y%m%d')
+            except ValueError:
+                pass
+            desc = re.sub("^Kortköp [0-9]{6}", "Kortköp", desc)
+            desc = re.sub("^Reservation Kortköp", "Kortköp", desc)
             datalist += [{'date': date, 'amount': amount, 'desc': desc}]
-print(sorted(datalist, key=lambda k: k['date']))
+#print(sorted(datalist, key=lambda k: k['date']))
+#for line in sorted(datalist, key=lambda k: k['date']):
+#    print("%s\t%.2f\t%s" % (line['date'].strftime("%Y-%m-%d"), line['amount'], line['desc']))
+print("=== Expenses ===")
+for line in sorted(datalist, key=lambda k: k['date']):
+    if line['amount'] < 0:
+        print("%s\t%.2f\t%s" % (line['date'].strftime("%Y-%m-%d"), -line['amount'], line['desc']))
+print()
+print("=== Income ===")
+for line in sorted(datalist, key=lambda k: k['date']):
+    if line['amount'] > 0:
+        print("%s\t%.2f\t%s" % (line['date'].strftime("%Y-%m-%d"), line['amount'], line['desc']))
+
